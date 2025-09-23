74355c50cfce7e65e846437f76ff4cdf62575e35
FauzieA <fauxieabdullahi22@gmail.com>
2025-09-24 01:46:49 +0800

    route changed to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-
-      {/* other routes */}
-    </Routes>
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* other routes */}
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
